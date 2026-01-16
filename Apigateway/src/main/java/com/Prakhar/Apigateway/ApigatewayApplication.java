42d1f9d321caf4465ed6f2d6d12764323787ddc9
Prakhar <prakharietdavv@gmail.com>
2026-01-16 15:33:10 +0530

    Final

--- Apigateway/src/main/java/com/Prakhar/Apigateway/ApigatewayApplication.java
+++ Apigateway/src/main/java/com/Prakhar/Apigateway/ApigatewayApplication.java
@@ -18,19 +18,5 @@ public class ApigatewayApplication {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        // Sirf tumhare React Port ko allow kar rahe hain
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.addAllowedMethod("*"); // GET, POST, PUT, DELETE sab chalega
-        corsConfig.addAllowedHeader("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
 }
 
